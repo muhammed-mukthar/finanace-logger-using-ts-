97f39c7f5bc12ca7afff18dad5bd31c590e81ac0
Muhammed Mukthar <muktharmanaf28@gmail.com>
2022-10-28 18:34:20 +0530

    tsc compile

---
+++ sandbox.js
@@ -0,0 +1,9 @@
+var character = "maker";
+console.log(character);
+var inputs = document.querySelectorAll("input");
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(inputs);
+});
+console.log('hallo');
+console.log("how are you");
---
+++ sandbox.ts
@@ -0,0 +1,13 @@
+const character = "maker";
+console.log(character);
+
+const inputs = document.querySelectorAll("input");
+
+console.log(inputs);
+
+inputs.forEach((input) => {
+  console.log(inputs);
+});
+
+console.log('hallo')
+console.log("how are you");
